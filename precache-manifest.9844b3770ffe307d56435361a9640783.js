b631d9a6ed76e8a648e7e7dbd56488a75c92ae42
patszy <patszy97@interia.pl>
2020-01-26 16:47:48 +0100

    Updates

---
+++ precache-manifest.9844b3770ffe307d56435361a9640783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08fc2ff2d575caba6d24627bd2be3b3",
+    "url": "/react-btc/index.html"
+  },
+  {
+    "revision": "c1e2c3bed016c1f3d1ff",
+    "url": "/react-btc/static/css/main.ab3d9c03.chunk.css"
+  },
+  {
+    "revision": "08701e7e3ba513a34205",
+    "url": "/react-btc/static/js/2.ad08e69a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-btc/static/js/2.ad08e69a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1e2c3bed016c1f3d1ff",
+    "url": "/react-btc/static/js/main.c2c45044.chunk.js"
+  },
+  {
+    "revision": "7a57f1324c08bb852d9e",
+    "url": "/react-btc/static/js/runtime-main.78565278.js"
+  }
+]);
